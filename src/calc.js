82d06ac2d8a7b7a9b998cc62140d86b4de6528f6
DANIEL PINHO MONTEIRO <DANMONTE@corp.ad.emb>
2018-01-04 09:47:32 -0200

    Ajustes lint

--- src/calc.js
+++ src/calc.js
@@ -1,6 +1,8 @@
 const sum = (num1, num2) => num1 + num2;
 const sub = (num1, num2) => num1 - num2;
 const mult = (num1, num2) => num1 * num2;
-const div = (num1, num2) => (num2 === 0) ? undefined : num1 / num2;
+const div = (num1, num2) => ((num2 === 0) ? undefined : num1 / num2);
 
-module.exports = { sum, sub, mult, div };
+module.exports = {
+  sum, sub, mult, div,
+};
